7dab40df351cbebafd30835946d4d4f63180d3a8
michelD-dev <delaunaymichel@hotmail.fr>
2022-11-03 22:07:50 +0100

    First commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test.todo('renders learn react link', () => {})
